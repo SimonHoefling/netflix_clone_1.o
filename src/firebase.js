3d36fbce4e60f85e94f3168cea81d5f9e44169ca
Simon Hoefling <simon@hoeflia.eu>
2023-09-16 09:40:56 +0700

    Initial commit
    Set up the app and deleted some stuff that i dont need

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVxdV4JqCv6YADkN4H5qqDlfr1XRAFp_0",
+  authDomain: "netflix-clone-ce139.firebaseapp.com",
+  projectId: "netflix-clone-ce139",
+  storageBucket: "netflix-clone-ce139.appspot.com",
+  messagingSenderId: "438387216378",
+  appId: "1:438387216378:web:d269868f33569a91171e43"
+};
