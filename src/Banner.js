6388078d0b5e4d4e53e591f6591241b7641b42b2
Simon Hoefling <simon@hoeflia.eu>
2023-09-16 13:55:31 +0700

    Added an API call to DMBD to get the movie covers, titles and descriptions

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Banner.css'
+import axios from './axios'
+import requests from './Requests'
 
 function Banner() {
+
+  const [movie, setMovie] = useState([])
+
+  // Function to fetch the movie data
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals)
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      )
+      return request;
+    }
+
+    fetchData()
+  }, [])
+
 
   // Function to truncate the description
   function truncate(str, n) {
@@ -13,18 +33,20 @@ function Banner() {
       className='banner'
       style={{
         backgroundSize: 'cover',
-        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: 'center center',
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className='banner__button'>Play</button>
           <button className='banner__button'>My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(`This is a test description`, 150)}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
 
